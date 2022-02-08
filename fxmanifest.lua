f9e84551bb61a447e01a45209cdfc082c537c801
skuud <99294756+skuud@users.noreply.github.com>
2022-02-08 22:59:42 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+name 'RealisticAirControl'
+
+author 'skuud'
+
+description 'Disable Air Control and Car Flip'
+
+version '1.0'
+
+client_script "client.lua"
+
